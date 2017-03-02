253baf536940a1679ccc3d4f135c1c40e5c34ad0
ahmed987 <ahmedsafe1998@gmail.com>
2017-03-02 10:52:51 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var a = 10;
+var b = 40;
+var c = 25;
+var solve = (a+b-c);
+alert ( solve);
+console.log (solve);
+
+var firstName = "ahmed";
+var lastName  = "feroz";
+var surName   = "khan";
+var complete  = firstName + " " + lastName + " " + surName  ;
+ alert ( complete );
+console.log (complete);
